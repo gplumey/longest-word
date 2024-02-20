b355678d019a9b78393a1d623ff899d16de0f87e
Guillaume Pumey <guillaume.plumey@gmail.com>
2024-02-20 18:43:17 +0000

    [RED]: implement is_valid

--- longest_word/game.py
+++ longest_word/game.py
@@ -1,9 +1,17 @@
+import random
+import string
+
 class Game:
     def __init__(self) -> list:
         """Attribute a random grid to size 9"""
-        self.grid = ['A','A','A','A','A','A','A','A', 'A']
+        self.grid = []
+        for _ in range(9):
+            self.grid.append(random.choice(string.ascii_uppercase))
         pass
 
     def is_valid(self, word: str) -> bool:
         """Return True if and only if the word is valid, given the Game's grid"""
-        pass # TODO
+        if len(word)> 0:
+            return True
+        else:
+            return False
